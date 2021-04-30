af9afbb2316c4f0a95f119cbb62e01f368200265
profjohnharney <john.harney@gmail.com>
2021-04-30 09:47:44 -0400

    getting going

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def main() -> None:
         screen_width,
         screen_height,
         tileset=tileset,
-        title="Yet Another Roguelike Tutorial",
+        title="Flotilla",
         vsync=True,
     ) as context:
         root_console = tcod.Console(screen_width, screen_height, order="F")
